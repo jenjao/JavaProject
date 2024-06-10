b3d5242c8a35d59ba1e2e0fe1d81da735c0e6d7f
jenjao <jaojen8@gmail.com>
2024-06-10 17:59:04 -0600

    add input from file.

--- MyJavaProgram/src/MyJavaProgram.java
+++ MyJavaProgram/src/MyJavaProgram.java
@@ -1,26 +1,46 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class MyJavaProgram {
 	// method - takes input and output
-	public static void main(String[] args) { // main(parameter)
-		System.out.println("Enter your name");
+	public static void main(String[] args) throws FileNotFoundException{ // main(parameter)
 
-        // input
-        Scanner in = new Scanner(System.in);
-        // take i/p from console and store in s
-        String s = in.nextLine();
-        // class -instantiation-> object
-        
-        if(s.equals("Jen")) {
-            System.out.println("Hey Jen");
-        } else if(s.equals("Frankie")) {
-            System.out.println("Hey Frankie");
-        } else {
-            System.out.println("You're not welcome here");
+//         Scanner in = new Scanner(System.in);
+// 
+//         System.out.println("Give us a size");
+//         int size = in.nextInt();
+// 
+//         System.out.println("Enter " + size + " numbers. Press enter after each");
+// 
+//         int[] grades = new int[size];
+// 
+//         for(int i = 0; i < size; i++) {
+//             grades[i] = in.nextInt();
+//         }
+// 
+//         in.close();
+// 
+//         for(int i = 0; i < size; i++) {
+//             System.out.println(grades[i]);
+//         }
 
+        Scanner in = new Scanner(new File("students.txt"));
+
+        List<String> students = new ArrayList<String>();
+
+        while(in.hasNextLine()) {
+            students.add(in.nextLine());
         }
-        
+
+        for(int i = 0; i < students.size(); i++) {
+            System.out.println("name: " + students.get(i));
+        }
+
         in.close();
+
 	}
 
 }

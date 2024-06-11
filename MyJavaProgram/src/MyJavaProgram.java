b56471c52a6ebac6112e1236fdef55746597f61e
jenjao <jaojen8@gmail.com>
2024-06-10 19:59:47 -0600

    implemeted queue

--- MyJavaProgram/src/MyJavaProgram.java
+++ MyJavaProgram/src/MyJavaProgram.java
@@ -1,46 +1,23 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Scanner;
 
 public class MyJavaProgram {
+
 	// method - takes input and output
-	public static void main(String[] args) throws FileNotFoundException{ // main(parameter)
+	public static void main(String[] args) throws FileNotFoundException {
+        // LinkedList as a queue
+        LinkedList<String> names = new LinkedList<String>();
+        names.add("Frankie");
+        names.add("Mochi");
+        names.add("Axel");
 
-//         Scanner in = new Scanner(System.in);
-// 
-//         System.out.println("Give us a size");
-//         int size = in.nextInt();
-// 
-//         System.out.println("Enter " + size + " numbers. Press enter after each");
-// 
-//         int[] grades = new int[size];
-// 
-//         for(int i = 0; i < size; i++) {
-//             grades[i] = in.nextInt();
-//         }
-// 
-//         in.close();
-// 
-//         for(int i = 0; i < size; i++) {
-//             System.out.println(grades[i]);
-//         }
-
-        Scanner in = new Scanner(new File("students.txt"));
-
-        List<String> students = new ArrayList<String>();
-
-        while(in.hasNextLine()) {
-            students.add(in.nextLine());
-        }
-
-        for(int i = 0; i < students.size(); i++) {
-            System.out.println("name: " + students.get(i));
-        }
-
-        in.close();
-
+        System.out.println(names.remove());
+        System.out.println(names.remove());
+        System.out.println(names.remove());
 	}
 
 }
